d592faf980094bc29c85ab9f2d56ebef694217ac
marcelcg8011 <86576646+marcelcg8011@users.noreply.github.com>
2022-04-06 21:16:25 +1200

    Initial commit

---
+++ src/com/company/School.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class School {
+    private String FullName;
+    School()
+    {
+
+    }
+    private void printFullName()
+    {
+        System.out.println("-------------- Task 1 --------------");
+        System.out.println("School Full Name: School of Mathematical Sciences");
+    }
+}
---
+++ src/com/company/UniversityOfferingSystem.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class UniversityOfferingSystem {
+
+    public static void main(String[] args) {
+	// write your code here{
+        //        FullName = "School of Mathematical and Computational Sciences";
+        //    }
+
+    }
+}
