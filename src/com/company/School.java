a1af987bdce223b922a4f23edc5f0f3dbfec2886
marcelcg8011 <86576646+marcelcg8011@users.noreply.github.com>
2022-04-06 21:23:38 +1200

    Main

--- src/com/company/School.java
+++ src/com/company/School.java
@@ -2,13 +2,23 @@ package com.company;
 
 public class School {
     private String FullName;
+
+    public School(String fullName) {
+        FullName = fullName;
+    }
     School()
     {
 
     }
-    private void printFullName()
+    public void printFullName()
     {
         System.out.println("-------------- Task 1 --------------");
-        System.out.println("School Full Name: School of Mathematical Sciences");
+        System.out.println("School Full Name: " + FullName);
+    }
+
+
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
     }
 }
--- src/com/company/UniversityOfferingSystem.java
+++ src/com/company/UniversityOfferingSystem.java
@@ -2,10 +2,10 @@ package com.company;
 
 public class UniversityOfferingSystem {
 
-    public static void main(String[] args) {
-	// write your code here{
-        //        FullName = "School of Mathematical and Computational Sciences";
-        //    }
+    public static void main(String[] args)
+    {
 
+        School ItDepartment = new School("School of Mathematical and Computational Sciences");
+        ItDepartment.printFullName();
     }
 }
